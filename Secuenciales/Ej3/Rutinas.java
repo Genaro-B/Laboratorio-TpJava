48f031657cb2c754e8578f624378caa55813c21a
Genaro-B <genaroivanbusto@gmail.com>
2024-10-13 16:09:02 -0300

    Se agregan los ejercicios con secuenciales hasta el ejercicio 5

---
+++ Secuenciales/Ej3/Rutinas.java
@@ -0,0 +1,21 @@
+package Ej3;
+
+import java.util.Scanner;
+
+public class Rutinas {
+    public static void main(String [] args){
+        /*
+            Enunciado:Escribe un programa que pida al usuario su nivel de condición física (principiante, intermedio,
+            avanzado) y luego genere una rutina de ejercicio semanal con diferentes tipos de ejercicios y
+            duraciones.
+         */
+        //Creamos una instancia de Scanner para leer los datos:
+        Scanner leer=new Scanner(System.in);
+        //Declaramos la variables a utilizar:
+        String condicionFisica="";
+        //Solicitar datos:
+        System.out.println("Ingrese su condición Fisica (Principiante,Intermedio,Avanzado):");
+
+
+    }
+}
